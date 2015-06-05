61f34b06861d0e80de7d08ca2320b63dbc543791
reevesrs24 <reevesrs24@yahoo.com>
2015-06-04 19:46:00 -0700

    Updated Basic Info tab
    -Basic Info tab now lets the user select the day in which the room is
    available and allows the user to individually assign the room hours
    -When the "add room" button is clicked all data will be displayed in the
    bottom list window
    -Many bugs need to be worked out i.e does not sanitize input data from
    the form fields

--- Project_S_GUI_Template/mainwindow.cpp
+++ Project_S_GUI_Template/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QString>
+#include <QDateTimeEdit>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +14,122 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+//Upon clicking the "add room" button, the dept/bldg input will be passed to the room variable
+void MainWindow::on_lineEdit_textChanged(const QString &arg1)
+{
+    room = arg1;
+}
+
+//Upon clicking the "add room" button, the listWidget will pass the variables to be displayed in the list
+void MainWindow::on_pushButton_5_clicked()
+{
+    ui->listWidget->insertItem(1, room);
+    ui->listWidget_2->insertItem(1, roomNumber);
+
+    ui->listWidget_3->insertItem(1, monday);
+    ui->listWidget_3->insertItem(2, tuesday);
+    ui->listWidget_3->insertItem(3, wednesday);
+    ui->listWidget_3->insertItem(4, thursday);
+    ui->listWidget_3->insertItem(5, friday);
+
+    ui->listWidget_4->insertItem(1, mEndTime);
+    ui->listWidget_4->insertItem(2, tEndTime);
+    ui->listWidget_4->insertItem(3, wEndTime);
+    ui->listWidget_4->insertItem(4, thEndTime);
+    ui->listWidget_4->insertItem(5, fEndTime);
+
+
+}
+/*
+ *  Check Box Widgets
+ **/
+
+//Passes the room number from the first line edit in Basic Info
+void MainWindow::on_lineEdit_2_textChanged(const QString &arg1)
+{
+    roomNumber = arg1;
+}
+
+//If Day is clicked overrides the initial N/A assignment
+void MainWindow::on_checkBox_clicked()
+{
+    monday = "Monday";
+}
+//If Day is clicked overrides the initial N/A assignment
+void MainWindow::on_checkBox_2_clicked()
+{
+    tuesday = "Tuesday";
+}
+//If Day is clicked overrides the initial N/A assignment
+void MainWindow::on_checkBox_3_clicked()
+{
+    wednesday = "Wednesday";
+}
+//If Day is clicked overrides the initial N/A assignment
+void MainWindow::on_checkBox_4_clicked()
+{
+    thursday = "Thursday";
+}
+//If Day is clicked overrides the initial N/A assignment
+void MainWindow::on_checkBox_5_clicked()
+{
+    friday = "Friday";
+}
+
+
+/*
+ *  Beginning and end time widgets for each day of the week
+ *  first camel case indicates day of the week followed by start and end times
+ **/
+//assigns beginning time
+void MainWindow::on_timeEdit_timeChanged(const QTime &time)
+{
+    mBegTime = time.toString();
+}
+//assigns ending time with beginning time to pass as the displayed time
+void MainWindow::on_timeEdit_2_timeChanged(const QTime &time)
+{
+    mEndTime = mBegTime + " - " + time.toString();
+}
+//assigns beginning time
+void MainWindow::on_timeEdit_5_timeChanged(const QTime &time)
+{
+    tBegTime = time.toString();
+}
+//assigns ending time with beginning time to pass as the displayed time
+void MainWindow::on_timeEdit_6_timeChanged(const QTime &time)
+{
+    tEndTime = tBegTime + " - " + time.toString();
+}
+//assigns beginning time
+void MainWindow::on_timeEdit_7_timeChanged(const QTime &time)
+{
+    wBegTime = time.toString();
+}
+//assigns ending time with beginning time to pass as the displayed time
+void MainWindow::on_timeEdit_8_timeChanged(const QTime &time)
+{
+    wEndTime = wBegTime + " - " + time.toString();
+}
+//assigns beginning time
+void MainWindow::on_timeEdit_9_timeChanged(const QTime &time)
+{
+    thBegTime = time.toString();
+}
+//assigns ending time with beginning time to pass as the displayed time
+void MainWindow::on_timeEdit_10_timeChanged(const QTime &time)
+{
+    thEndTime = thBegTime + " - " + time.toString();
+}
+//assigns beginning time
+void MainWindow::on_timeEdit_11_timeChanged(const QTime &time)
+{
+    fBegTime = time.toString();
+}
+//assigns ending time with beginning time to pass as the displayed time
+void MainWindow::on_timeEdit_12_timeChanged(const QTime &time)
+{
+    fEndTime = fBegTime + " - " + time.toString();
 }
--- Project_S_GUI_Template/mainwindow.h
+++ Project_S_GUI_Template/mainwindow.h
@@ -1,8 +1,11 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
+#include <QTime>
+#include <QString>
+#include <string.h>
 
+using namespace std;
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +18,74 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    //Room name variable
+    QString room;
+    //Room number variable
+    QString roomNumber;
+
+    //Day Variables
+    QString monday = "Monday";
+    QString tuesday = "Tuesday";
+    QString wednesday = "Wednesday";
+    QString thursday = "Thursday";
+    QString friday = "Friday";
+    QString saturday = "Saturday";
+    QString sunday = "Sunday";
+
+    //Time Variables
+    QString mBegTime;
+    QString mEndTime = "N/A";
+    QString tBegTime;
+    QString tEndTime = "N/A";
+    QString wBegTime;
+    QString wEndTime = "N/A";
+    QString thBegTime;
+    QString thEndTime = "N/A";
+    QString fBegTime;
+    QString fEndTime = "N/A";
+
+private slots:
+
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+    void on_pushButton_5_clicked();
+
+    void on_lineEdit_2_textChanged(const QString &arg1);
+
+    void on_checkBox_clicked();
+
+    void on_checkBox_2_clicked();
+
+    void on_checkBox_3_clicked();
+
+    void on_checkBox_4_clicked();
+
+    void on_timeEdit_timeChanged(const QTime &time);
+
+    void on_timeEdit_2_timeChanged(const QTime &time);
+
+    void on_timeEdit_5_timeChanged(const QTime &time);
+
+    void on_timeEdit_6_timeChanged(const QTime &time);
+
+    void on_timeEdit_7_timeChanged(const QTime &time);
+
+    void on_timeEdit_8_timeChanged(const QTime &time);
+
+    void on_timeEdit_9_timeChanged(const QTime &time);
+
+    void on_timeEdit_10_timeChanged(const QTime &time);
+
+    void on_checkBox_5_clicked();
+
+    void on_timeEdit_11_timeChanged(const QTime &time);
+
+    void on_timeEdit_12_timeChanged(const QTime &time);
+
 private:
     Ui::MainWindow *ui;
+
 };
 
 #endif // MAINWINDOW_H
