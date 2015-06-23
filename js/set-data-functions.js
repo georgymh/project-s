ae1b0895de053f0d202a1b66d822b04260f95bfb
reevesrs24 <reevesrs24@yahoo.com>
2015-06-22 23:01:36 -0700

    Inorporated user data into JSON format

---
+++ js/set-data-functions.js
@@ -0,0 +1,30 @@
+
+
+
+
+function setDepartmentData() {
+    
+        userData.department = document.getElementById("deptName").value;
+        userData.building = document.getElementById("bldgName").value;
+        userData.monOpeningHours =  document.getElementById("monday-starting-hours").value;
+        userData.monClosingHours = document.getElementById("monday-ending-hours").value;
+        userData.tuesOpeningHours = document.getElementById("tuesday-starting-hours").value;
+        userData.tuesClosingHours = document.getElementById("tuesday-ending-hours").value;
+        userData.wedOpeningHours = document.getElementById("wednesday-starting-hours").value;
+        userData.wedClosingHours = document.getElementById("wednesday-ending-hours").value;
+        userData.thursOpeningHours = document.getElementById("thursday-starting-hours").value;
+        userData.thursClosingHours = document.getElementById("thursday-ending-hours").value;
+        userData.friOpeningHours = document.getElementById("friday-starting-hours").value;
+        userData.friClosingHours = document.getElementById("friday-ending-hours").value;
+        userData.satOpeningHours = document.getElementById("saturday-starting-hours").value;
+        userData.satClosingHours = document.getElementById("saturday-ending-hours").value;
+        userData.sunopeningHours = document.getElementById("sunday-starting-hours").value;
+        userData.sunClosingHours = document.getElementById("sunday-ending-hours").value;
+                
+}
+    
+//Test JSON USER Data
+function setDeptData() {
+    
+    alert(userData.sunClosingHours);
+}
---
+++ js/user-data.js
@@ -0,0 +1,21 @@
+var userData = {
+    
+    department: "N/A",
+    building: "N/A",
+        
+    monOpeningHours : "N/A",
+    monClosingHours : "N/A",
+    tuesOpeningHours : "N/A",
+    tuesClosingHours : "N/A",
+    wedOpeningHours : "N/A",
+    wedClosingHours : "N/A",
+    thursOpeningHours : "N/A",
+    thursClosingHours : "N/A",
+    friOpeningHours : "N/A",
+    friClosingHours : "N/A",
+    satOpeningHours : "N/A",
+    satClosingHours : "N/A",
+    sunClosingHours : "N/A",
+      
+};
+
