c304abece9ef931d16524bcc7a249c63c33d00d1
Georgy Marrero <geoxxstyle@gmail.com>
2015-08-10 20:45:45 -0700

    Clean up progress form js

--- js/progress-form.js
+++ js/progress-form.js
@@ -55,95 +55,33 @@ jQuery(document).ready(function() {
 
 });
 
-    var __handleSubmit = function HandleSubmit(replyObj) {
-        if (replyObj == 1) {
-            //showSuccessMessage();
-        } else {
-            //showErrorAlert(replyObj);
-        }
+var __handleSubmit = function HandleSubmit(replyObj) {
+    if (replyObj == 1) {
+        //showSuccessMessage();
+    } else {
+        //showErrorAlert(replyObj);
+    }
 
-        //console.log(replyObj);
-    };
+    //console.log(replyObj);
+};
 
-    function getCurrentStep() {
-	    if ( $('#step-one').is(":visible") ) {
-	        return $('#step-one'); // step 1
+function getCurrentStep() {
+    if ( $('#step-one').is(":visible") ) {
+        return $('#step-one'); // step 1
 
-	    } else if ( $('#step-two').is(":visible") ) {
-	        return $('#step-two'); // step 2
+    } else if ( $('#step-two').is(":visible") ) {
+        return $('#step-two'); // step 2
 
-	    } else if ( $('#step-three').is(":visible") ) {
-	        return $('#step-three'); // step 3
+    } else if ( $('#step-three').is(":visible") ) {
+        return $('#step-three'); // step 3
 
-	    } else if ( $('#step-four').is(":visible") ) {
-	        return $('#step-four'); // step 4
+    } else if ( $('#step-four').is(":visible") ) {
+        return $('#step-four'); // step 4
 
-	    } else {
-	        return null; // unknown
-	        console.log("ERROR: erroneous step!!! Don't let this bug spread.")
-	    }
-	}
-
-
+    } else {
+        return null; // unknown
+        console.log("ERROR: erroneous step!!! Don't let this bug spread.")
+    }
+}
 
 
-
-	// $(document).ready(function(){
-	// 	var current = 1;
-		
-	// 	widget      = $(".step");
-	// 	btnnext     = $(".next");
-	// 	btnback     = $(".back"); 
-	// 	btnsubmit   = $(".submit");
-
-	// 	// Init buttons and UI
-	// 	widget.not(':eq(0)').hide();
-	// 	hideButtons(current);
-	// 	setProgress(current);
-
-	// 	// Next button click action
-	// 	btnnext.click(function(){
-	// 		if(current < widget.length){
-	// 			widget.show();
-	// 			widget.not(':eq('+(current++)+')').hide();
-	// 			setProgress(current);
-	// 		}
-	// 		hideButtons(current);
-	// 	})
-
-	// 	// Back button click action
-	// 	btnback.click(function(){
-	// 		if(current > 1){
-	// 			current = current - 2;
-	// 			btnnext.trigger('click');
-	// 		}
-	// 		hideButtons(current);
-	// 	})			
-	// });
-
-	// // Change progress bar action
-	// setProgress = function(currstep){
-	// 	var percent = parseFloat(100 / widget.length) * currstep;
-	// 	percent = percent.toFixed();
-	// 	$(".progress-bar").css("width",percent+"%").html(percent+"%");		
-	// }
-
-	// // Hide buttons according to the current step
-	// hideButtons = function(current){
-	// 	var limit = parseInt(widget.length); 
-
-	// 	$(".action").hide();
-
-	// 	if(current < limit) btnnext.show();
-	// 	if(current > 1) btnback.show();
-	// 	if (current == limit) { btnnext.hide(); btnsubmit.show(); }
-	// }
- //  var _gaq = _gaq || [];
- //  _gaq.push(['_setAccount', 'UA-19096935-1']);
- //  _gaq.push(['_trackPageview']);
-
- //  (function() {
- //    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
- //    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
- //    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
- //  })();
