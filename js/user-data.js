b682885d8244977744802b46e10ae6c6b7f5f3a0
Georgy Marrero <geoxxstyle@gmail.com>
2015-08-10 20:56:00 -0700

    Fix array on hours of operation JSON object

--- js/user-data.js
+++ js/user-data.js
@@ -1,11 +1,10 @@
-var userData = {
-    
-        "department": "N/A",
-        "building": "N/A", 
-       
-    
-        "hours":[
-            {
+var data = {
+
+    // Step One: Department Information
+    "stepOne" : {
+        "department" : "N/A",
+        "building" : "N/A", 
+        "operatingHours": {
                 monOpeningHours : "N/A",
                 monClosingHours : "N/A",
                 tuesOpeningHours : "N/A",
@@ -19,10 +18,19 @@ var userData = {
                 satOpeningHours : "N/A",
                 satClosingHours : "N/A",
                 sunOpeningHours : "N/A",
-                sunClosingHours : "N/A",
-            }
-        ]
-    
+                sunClosingHours : "N/A",   
+        }
+    },
+
+    // Step Two: Room Information
+    "stepTwo" : {
+
+    },
+
+    // Step Three: Instructor & Classes Information
+    "stepThree" : {
+
+    }
         
 };
 
