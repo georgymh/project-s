ad0e862a3ae23ddee3899309b20d6dfde4e98f65
Georgy Marrero <geoxxstyle@gmail.com>
2015-08-16 16:53:08 -0700

    Add code that populates Department and Room classes.

--- process.php
+++ process.php
@@ -38,5 +38,103 @@
 		// 			$name = "Class Name";
 		//			$start = "2015-08-10T10:10:00";
 		//			$end = "2015-08-10T12:00:00";
+	
+	include_once 'php/DaysOfWeek.php';
+
+	// Set error logs options.
+	ini_set("log_errors", 1);
+	ini_set("error_log", __DIR__ . "/tmp/php-error.log");
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+		// 0. Pre routines: Get the POST data and verify it's set.
+		if (isset($_POST["data"]) && !empty($_POST["data"])) {
+			$data = $_POST["data"];
+		} else {
+			echo 0;
+			exit(); 
+		}
+
+		//print_r ($data);
+
+		// 1. Put JSON data into variables.
+		
+		// 1.a. Step One - Building/Department info
+		$stepOneData = $data["stepOne"];
+		$departmentName = $stepOneData["department"];
+		$buildingName = $stepOneData["building"];
+		$operatingHours = $stepOneData["operatingHours"]; // array
+		$mondayHours = array("open" => $operatingHours["monOpeningHours"],
+							 "close" => $operatingHours["monClosingHours"]);
+		$tuesdayHours = array("open" => $operatingHours["tuesOpeningHours"],
+							 "close" => $operatingHours["tuesClosingHours"]);
+		$wednesdayHours = array("open" => $operatingHours["wedOpeningHours"],
+							 "close" => $operatingHours["wedClosingHours"]);
+		$thursdayHours = array("open" => $operatingHours["thursOpeningHours"],
+							 "close" => $operatingHours["thursClosingHours"]);
+		$fridayHours = array("open" => $operatingHours["friOpeningHours"],
+							 "close" => $operatingHours["friClosingHours"]);
+		$saturdayHours = array("open" => $operatingHours["satOpeningHours"],
+							 "close" => $operatingHours["satClosingHours"]);
+		$sundayHours = array("open" => $operatingHours["sunOpeningHours"],
+							 "close" => $operatingHours["sunClosingHours"]);
+		$operatingDaysWithHours = [DaysOfWeek::Monday => $mondayHours, 
+								   DaysOfWeek::Tuesday => $tuesdayHours, 
+								   DaysOfWeek::Wednesday => $wednesdayHours,
+								   DaysOfWeek::Thursday => $thursdayHours, 
+								   DaysOfWeek::Friday => $fridayHours, 
+								   DaysOfWeek::Saturday => $saturdayHours,
+								   DaysOfWeek::Sunday => $sundayHours];
+
+		// 1.b. Step Two - Room info
+		$stepTwoData = $data["stepTwo"];
+		$rooms = $stepTwoData["rooms"];
+
+		// 1.c Step Three - Instructor and Classes info
+		
+		// 1.d (optional) Print the variables.
+		// echo "========= TESTING (PRINTING) =========\n";
+		// echo "Department name: " . $departmentName . "\n";
+		// echo "Building name: " . $buildingName . "\n";
+		// $daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+		// 				"Saturday", "Sunday"];
+		// echo "Operating Hours: \n";
+		// for ($i = 0; $i < count($daysOfWeek); $i++) {
+		// 	$dayWithHours = $operatingDaysWithHours[$i];
+		// 	echo $daysOfWeek[$i] . ": " . $dayWithHours["open"] . " to " . $dayWithHours["close"];
+		// 	echo "\n";
+		// }
+		// echo "Rooms (count: " . count($rooms) . "): \n";
+		// foreach($rooms as $room) {
+		// 	echo $room . "\n";
+		// }
+
+		// 2. Store information into Input Classes.
+		// 2.a Store the info.
+		include 'php/IncludeInputClasses.php';
+
+		$department = new Department();
+		$department->setName($departmentName);
+		foreach($operatingDaysWithHours as $day => $hours) {
+			if ($hours["open"] != null && $hours["close"] != null){
+				// The day is set.
+				$hoursOfOperation = TimeInterval::create()->withStart($hours["open"])->withEnd($hours["close"]);
+				$department->addOperationDayAndTime( $day, $hoursOfOperation );
+			} else {
+				// The day is not set -- we skip it.
+			}
+		}
+
+		foreach($rooms as $room) {
+			$prefixAndNumber = split(" ", $room);
+			$department->addRoom( Room::create()->withPrefix($prefixAndNumber[0])->withRoomNumber($prefixAndNumber[1]) );
+		}
+
+		// 2.b. Verify everything is full/with no errors.
+		// PROBABLY WON'T HAVE THIS ON VERSION 0.1.
+
+		// 2.c. (optional) Print the data.
+		$department->printObject();
+	}
 
 ?>
