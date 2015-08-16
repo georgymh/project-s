1988dfa4e5ce32403cfb4fa2df83ca9b9643c713
Georgy Marrero <geoxxstyle@gmail.com>
2015-08-16 16:51:48 -0700

    Retouch Department Class

--- php/Department.php
+++ php/Department.php
@@ -8,75 +8,21 @@
 // Inclusions.
 require_once('DaysOfWeek.php');
 require_once('TimeInterval.php');
+require_once('AvailSchedule.php');
 
 class Department {
 // PUBLIC:
 
-	/*
-	* Overloaded Constructor Hack (A mix of factory and fluent interface style)
-	*/
-
-		// Static constructor / Factory
-		public static function create() {
-			$instance = new self();
-			$thilistOfRooms = new SplDoublyLinkedList();
-			return $instance;
-		}
-
-		// Name setter - fluent style
-		public function withName( $name ) {
-			$this->name = $name;
-			return $this;
-		}
-
-		// Days Opened setter - fluent style
-		public function withDaysOpened( $days ) {
-			$this->daysOpened = $days;
-			return $this;
-		}
-
-		// Hours Opened setter - fluent style
-		public function withHoursOpened( $hours ) {
-			$this->hoursOpened = $hours;
-			return $this;
-		}
-
-		// List of Rooms setter - fluent style
-		public function withListOfRooms( $rooms ) {
-			$this->listOfRooms = $rooms;
-			return $this;			
-		}
-
-
 	// Normal Default Constructor
-	
 	function __construct() {
-		$departmentName = "NO_DEPARTMENT_NAME";
+		$this->departmentName = "NO_DEPARTMENT_NAME";
 
-		$daysOpened = [
-			DaysOfWeek::Monday 		=> FALSE,
-			DaysOfWeek::Tuesday 	=> FALSE,
-			DaysOfWeek::Wednesday 	=> FALSE,
-			DaysOfWeek::Thursday 	=> FALSE,
-			DaysOfWeek::Friday 		=> FALSE,
-			DaysOfWeek::Saturday 	=> FALSE,
-			DaysOfWeek::Sunday 		=> FALSE
-		];
+		$this->operationSchedule = new DepartmentAvailSchedule();
 
-		$emptyTimeInterval = new TimeInterval();
-
-		$hoursOpened = [
-			DaysOfWeek::Monday 		=> $emptyTimeInterval,
-			DaysOfWeek::Tuesday 	=> $emptyTimeInterval,
-			DaysOfWeek::Wednesday 	=> $emptyTimeInterval,
-			DaysOfWeek::Thursday 	=> $emptyTimeInterval,
-			DaysOfWeek::Friday 		=> $emptyTimeInterval,
-			DaysOfWeek::Saturday 	=> $emptyTimeInterval,
-			DaysOfWeek::Sunday 		=> $emptyTimeInterval
-		];
-
-		$listOfRooms = new SplDoublyLinkedList();
+		$this->listOfRooms = new SplDoublyLinkedList();
 	}
+
+	// Useful methods
 
 	/**
 	 * Adds a room to the bottom of the list of rooms.
@@ -88,6 +34,39 @@ class Department {
 		}
 
 		$this->listOfRooms->push( $room );
+	}
+
+	/**
+	 * Adds a day and hours in which the department and its rooms will operate.
+	 * 
+	 * @param DaysOfWeek $day   		Days of operation
+	 * @param TimeInterval $interval  	Hours of operation
+	 */
+	public function addOperationDayAndTime( $day, $interval ) {
+		$this->operationSchedule->setAvailableTimeInterval( $day, $interval );
+	}
+
+	// Setters
+	
+	/**
+	* Sets the name of the department that manages all the rooms.
+	*
+	* @param string $newName The name of the department.
+	*/
+	public function setName( $newName ) {
+		$this->name = $newName;
+	}
+
+	/**
+	* Sets the hours of every day in which the department (and its rooms) will 
+	* operate.
+	*
+	* NOTE: THIS IS AN OPTIONAL METHOD.
+	*
+	* @param DepartmentAvailSchedule $newSchedule
+	*/
+	public function setOperationSchedule ( $newSchedule ) {
+		$this->operationSchedule = $newSchedule;
 	}
 
 	// Getters
@@ -108,43 +87,6 @@ class Department {
 		return $this->listOfRooms;
 	}
 
-	// Setters
-	
-	/*
-	* Sets the name of the department that manages all the rooms.
-	*
-	* @param string $newName The name of the department.
-	*/
-	public function setName( $newName ) {
-		$this->name = $newName;
-	}
-
-	/*
-	* Sets the days in which the department (and its rooms) will be open.
-	*
-	* @param [DaysOfWeek => boolean] An associative array containing each
-	*                       	     day of the week as keys, and booleans
-	*                       	     as values (representing if the room is
-	*                       	     operating on that day or not).
-	*                          
-	*/
-	public function setDaysOpened ( $newDaysOpened ) {
-		$this->daysOpened = $newDaysOpened;
-	}
-
-	/*
-	* Sets the hours of every day in which the department (and its rooms) will 
-	* operate.
-	*
-	* @param [DaysOfWeek => TimeInterval] An associative array containing each day 
-	*                       		      of the week as keys, and time INTERVALS
-	*                       		      as values that represent the interval of 
-	*                       		      time that the rooms will operate per day.
-	*/
-	public function setHoursOpened ( $newHoursOpened ) {
-		$this->hoursOpened = $newHoursOpened;
-	}
-
 	// Other methods
 	
 	/*
@@ -155,15 +97,15 @@ class Department {
 		echo "*** Printing the object *** <br><br>";
 		echo "<b>Building name:</b> " . $this->name . " <br>";
 
-		echo "<b>Days Opened:</b> <br>";
-		// Print associative array.
-		print_r( $this->daysOpened );
-		echo "<br>";
+		echo "<b>Operation Schedule:</b> <br>";
+		$this->operationSchedule->printObject();
 
-		echo "<b>Hours opened:</b> <br>";
-		print_r( $this->hoursOpened );
+		echo "<b>List of rooms (count: ". count($this->listOfRooms) ."):</b> <br>";
+		foreach($this->listOfRooms as $room) {
+			$room->printObject();
+		}
+		
 		echo "<br><hr><br><br>";
-
 	}
 
 // PRIVATE:
@@ -175,20 +117,13 @@ class Department {
 	private $name;
 
 	/**
-	* Represents the days of the week that the department and its rooms are 
-	* operating.
+	* Represents the days and hours of the week that the department and its 
+	* rooms are operating.
 	*
-	* @var associative array [DaysOfWeek => boolean]
+	* @var DepartmentAvailSchedule
 	*/
-	private $daysOpened;
+	private $operationSchedule;
 
-	/**
-	* Represents the hours of the week that the department and its rooms 
-	* are operating.
-	* 
-	* @var associative array [DaysOfWeek => TimeInterval]
-	*/
-	private $hoursOpened;
 
 	/**
 	 * The list of rooms that belong to a department.
