1ab115982c13269f6f422274f4de8595c755413e
reevesrs24 <reevesrs24@yahoo.com>
2015-08-16 16:56:27 -0700

    Added delete button to edit modal

--- js/instructor-table.js
+++ js/instructor-table.js
@@ -7,6 +7,18 @@ var editID;
 var totalClass = 0;
 var u_ID = 0;
 
+
+/*
+    Function that deletes the class that the user has added in step 2
+*/
+var deleteClass = function() {
+    var listItem = this.parentNode;
+    var ul = listItem.parentNode;
+    ul.removeChild(listItem);
+    
+    totalClass--;
+}
+
 /*
     Function that sets instructor data from modal into the step 3 form
 */
@@ -157,13 +169,25 @@ var editInstructorInfo = function() {
     
         var newList = document.createElement("li");
         newList.class = "text-center";
-        var arrayVariables = document.createTextNode(totalClasses[i]);
+        
+        //create new delete button to be appended
+        var deleteRoomBtn = document.createElement("button");
+        var deleteText = document.createTextNode("×");
+        deleteRoomBtn.appendChild(deleteText);
+        newList.appendChild(deleteRoomBtn);
+        
+        var arrayVariables = document.createTextNode(totalClasses[i] + " ");
         newList.appendChild(arrayVariables)
+        newList.appendChild(deleteRoomBtn);
         
         var currentList = document.getElementById("class-list");
         currentList.appendChild(newList);
         totalClass++;
     }
+    
+    deleteRoomBtn.type = "button";
+    deleteRoomBtn.id = "delete-room-btn";
+    deleteRoomBtn.onclick = deleteClass;
     
     editBool = true;
     editID = this.id;
@@ -192,3 +216,55 @@ function storeClasses() {
     }
     instructorData.classes.push(classArray);
 }
+
+/*
+    Function that inserts new classes added in Step 3
+*/
+
+function insertClass() {
+    
+     if(document.getElementById('class-title').value === null || document.getElementById('class-title').value === ''){
+        //alert("You must enter a room prefix");
+    }  else{ 
+    var classTitle = document.getElementById("class-title").value;
+    var credit = document.getElementById("hours").value;
+    var freq = document.getElementById("frequency").value;
+    
+    //reset all values
+    document.getElementById("instructor-classes").innerHTML = '';
+    document.getElementById("hours").value;
+    document.getElementById("frequency").value;
+        
+    //create new list element
+    var newClassList = document.createElement("li");
+    newClassList.class = "text-center";
+    
+    //create new elements to be appended
+    var deleteRoomBtn = document.createElement("button");
+    var classListText = document.createTextNode(classTitle + " " + credit + " " + freq);
+    var deleteText = document.createTextNode("×");
+    
+    //aggregrate new elemenets into one element
+    newClassList.appendChild(classListText);
+    deleteRoomBtn.appendChild(deleteText);
+    newClassList.appendChild(deleteRoomBtn);
+    
+    var currentClassList = document.getElementById("class-list");
+    currentClassList.appendChild(newClassList);
+    
+    deleteRoomBtn.type = "button";
+    deleteRoomBtn.id = "delete-room-btn";
+    deleteRoomBtn.onclick = deleteClass;
+    
+    document.getElementById("class-title").value = '';
+    document.getElementById("hours").value = '';
+    document.getElementById("frequency").value = '';
+    
+    totalClass++;
+        
+    console.log(credit);
+    }
+    
+}
+
+
