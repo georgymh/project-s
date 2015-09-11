3906f2c8c19b15b59d3b577a0eaa93f3ec7972c4
Georgy Marrero <geoxxstyle@gmail.com>
2015-09-10 21:37:12 -0700

    Add functions to manage instructor data + missing routine

--- resources/js/instructordata.js
+++ resources/js/instructordata.js
@@ -65,3 +65,69 @@ $(document).ready(function() {
 function getTypeOfInstructor() {
 	return $('#type-full-time').is(':checked') ? 'fulltime' : 'parttime';
 }
+/* Functions to manage the instructor data
+*************************************************/
+
+// On instructor click
+$(document).ready(function() {
+	$('#instructor-list').on('click', 'a', function(e) {
+		e.preventDefault();
+
+		// Get instructor from option and verify it's valid
+		var fullName = $(this).text();
+		var instructor = getInstructorFromFullName(fullName);
+		console.log(instructor);
+		if (instructor == null) {
+			return false;
+		}
+
+		// Change current instructor
+		$('#current-instructor').html(fullName);
+
+		// Load data related to this instructor (e.g. its classes)
+		loadInstructor(instructor);
+	});
+});
+
+function loadInstructor(instructor) {
+	
+}
+
+/* Other functions
+************************************************/
+
+function getCurrentInstructor() {
+	var fullName = $('#current-instructor').text();
+	return getInstructorFromFullName(fullName);
+}
+
+function getInstructorFromFullName(fullName) {
+	fullName = fullName.split(',');
+	var firstName = fullName[1].trim();
+	var lastName = fullName[0].trim();
+
+	var instructor = null
+	$.each(instructorData, function(key, value) {
+		console.log(value.lastName);
+		if (value.firstName == firstName && value.lastName == lastName) {
+			instructor = value;
+			return false;
+		}
+	});
+
+	return instructor;
+}
+
+function generateUUID(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+function sanitizeText(text) {
+	return text.replace(/[^\w\s]/gi, '');
+}
