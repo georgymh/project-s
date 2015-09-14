612da0a91a3bb529b61ebf210734194696de2848
Georgy Marrero <geoxxstyle@gmail.com>
2015-09-13 22:56:04 -0700

    Add new instructor to test

--- resources/js/schedule-testing-mode.js
+++ resources/js/schedule-testing-mode.js
@@ -7,4 +7,13 @@ $(document).ready(function() {
 	$('#class-total-hours').val('90');
 	$('option[value=2]').attr('selected', 'selected');
 	$('#add-class-btn').click();
+
+	$('#first-name').val('Justin');
+	$('#last-name').val('Jang');
+	$('#add-instructor-btn').click();
+
+	$('#class-title').val('CS A170');
+	$('#class-total-hours').val('70');
+	$('option[value=2]').attr('selected', 'selected');
+	$('#add-class-btn').click();
 });
