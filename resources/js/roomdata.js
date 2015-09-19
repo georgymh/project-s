0db6a19a98c147c65ff66ad8637682a3ee0a1cdc
Georgy Marrero <geoxxstyle@gmail.com>
2015-09-18 20:21:37 -0700

    Clean roomdata script

--- resources/js/roomdata.js
+++ resources/js/roomdata.js
@@ -1,11 +1,11 @@
+/* Initialization routines
+-----------------------------------------------------------------*/
 
 $(document).ready(function() {
 // Draw the room boxes
 drawRoomBoxes(JSONData.rooms);
 
-/* Activate room boxes click
------------------------------------------------------------------*/
-
+// Activate room boxes click interaction
 $('.room-container').on('click', function() {
 	$('.right-side-box').find('.room-container').each(function() {
 		$(this).removeClass('current');
@@ -16,11 +16,14 @@ $('.room-container').on('click', function() {
 	$('#current-room').text($(this).text());		
 });
 
-// Activate first room box
+// Click/Choose the first room
 $('.room-container').first().click();
 
 });
 
+// HELPER METHODS TO Initialization routines
+
+// Function that draws the room boxes
 function drawRoomBoxes(rooms) {
 	//console.log(rooms);
 	var roomBoxes = '';
