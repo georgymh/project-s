756cb4ba3624e171d5e6b4c8c74c889b0189f557
Georgy Marrero <geoxxstyle@gmail.com>
2015-09-24 18:23:10 -0700

    Cleanup index

--- index.php
+++ index.php
@@ -12,27 +12,16 @@
 
     switch($CURRENT_VIEW) {
         case 'steps':
-            //echo '- steps... ';
             MainUI::showSteps();
         break;
         case 'schedule':
-            //echo '- schedule... ';
-
             if (isset($_SESSION['stepsData'])) {
-                //echo 'data is set! -- drawing schedule';
-
-                // $data = $_POST['data'];
-                // $roomCount = $data['rooms']->length;
                 MainUI::showSchedule();
             } else {
-                //echo 'but data isn\'t set -- drawing steps';
-
                 MainUI::showSteps();
             }
         break;
         default:
-            //echo 'default case!!! ';
-
             MainUI::showSteps();
         break;
     }
